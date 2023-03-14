002e1a3855a9550b691b4c568472889f9cd7b683
andrey lazar <andreylazarmr@gmail.com>
2023-03-14 17:58:13 +0300

    task54, task56, task58

--- Task56/Program.cs
+++ Task56/Program.cs
@@ -1,13 +1,11 @@
-﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию 
-// элементы каждой строки двумерного массива.
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, 
+// которая будет находить строку с наименьшей суммой элементов.
 // Например, задан массив:
 // 1 4 7 2
 // 5 9 2 3
 // 8 4 2 4
-// В итоге получается вот такой массив:
-// 7 4 2 1
-// 9 5 3 2
-// 8 4 4 2
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
 
 Console.Clear();
 
@@ -18,7 +16,9 @@ int max = Prompt("Введите максимальное значение в м
 int[,] array = FillArray(row, column, min, max);
 PrintArray(array);
 Console.WriteLine();
-PrintArray(GetStreamline(array));
+GetSumma(array);
+
+
 
 int Prompt(string text)
 {
@@ -48,28 +48,29 @@ void PrintArray(int[,] arr)
     {
         for (int j = 0; j < arr.GetLength(1); j++)
         {
-            Console.Write(arr[i, j] + $"  ");
+            Console.Write(arr[i, j] + " ");
         }
         Console.WriteLine();
     }
 }
 
-int[,] GetStreamline(int[,] InputArray)
+void GetSumma(int[,] inputArray)
 {
-    for (int i = 0; i < InputArray.GetLength(0); i++)
+    int t = int.MaxValue;
+    int count = 0;
+    for (int i = 0; i < inputArray.GetLength(0); i++)
     {
-        for (int j = 0; j < InputArray.GetLength(1); j++)
+        int summa = 0;
+        for (int j = 0; j < inputArray.GetLength(1); j++)
         {
-            for (int k = 0; k < InputArray.GetLength(1)-1; k++)
-            {
-            if (InputArray[i, k] < InputArray[i, k+1])
-            {
-                int temp = InputArray[i, k];
-                InputArray[i, k] = InputArray[i, k + 1];
-                InputArray[i, k + 1] = temp;
-            }
-            }
+            summa += inputArray[i, j];
+        }
+        Console.WriteLine($"Сумма элементов в {i + 1} строке равна " + summa);
+        if (t > summa)
+        {
+            t = summa;
+            count = i + 1;
         }
     }
-    return InputArray;
+    Console.WriteLine($"наименьшая сумма элементов в  строке " + count);
 }
---
+++ Task58/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+Console.Clear();
+
+int[,] arrayFirst = FillArray(2, 3, 1, 9);
+int[,] arraySecond = FillArray(3, 2, 1, 9);
+PrintArray(arrayFirst);
+PrintArray(arraySecond);
+PrintArray(GetProductTwoMatrices(arrayFirst, arraySecond));
+
+int[,] FillArray(int rowArray, int columnArray, int min, int max)
+{
+    int[,] resultArray = new int[rowArray, columnArray];
+    Random rnd = new Random();
+    for (int i = 0; i < rowArray; i++)
+    {
+        for (int j = 0; j < columnArray; j++)
+        {
+            resultArray[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return resultArray;
+}
+
+void PrintArray(int[,] arr)
+{
+    Console.WriteLine("----------------------------");
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] GetProductTwoMatrices(int[,] arrayA, int[,] arrayB)
+{
+    int aRows = arrayA.GetLength(0); int aCols = arrayA.GetLength(1);
+    int bRows = arrayB.GetLength(0); int bCols = arrayB.GetLength(1);
+    if (aCols != bRows)
+    new Exception("Матрицы не соответствуют");
+    int[,] result = new int[aRows, bCols];
+    for (int i = 0; i < aRows; i++)
+    {
+        for (int j = 0; j < bCols; j++)
+        {
+            for (int k = 0; k < aCols; ++k)
+            result[i, j] += arrayA[i, k] * arrayB[k, j];
+        }
+    }
+    return result;
+}
